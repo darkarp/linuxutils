3e49e251fb6d964f48595e356ff887d2907c6cf8
Mário Nascimento <mario@whitehathacking.tech>
2024-03-03 02:33:48 +0000

    fix numbering

--- src/main.c
+++ src/main.c
@@ -42,12 +42,12 @@ int main(int argc, char *argv[]) {
                    "list\n  %s search <keyword>\n",
                    argv[0], argv[0], argv[0]);
         }
-    } else if (strcmp(baseName, "fsearch") == 0 && argc == 4) {
+    } else if (strcmp(baseName, "fsearch") == 0 && argc == 3) {
         run_fsearch(argv[2], argv[3]);
-    } else if (strcmp(baseName, "cat") == 0 && argc == 3) {
+    } else if (strcmp(baseName, "cat") == 0 && argc == 2) {
         run_cat(argv[2]);
     } else if (strcmp(baseName, "ln") == 0) {
-        if (argc != 5) {
+        if (argc != 4) {
             printf("Usage: %s ln [-s] <target> <linkname>\n", argv[0]);
             return 1;
         }
@@ -55,9 +55,9 @@ int main(int argc, char *argv[]) {
         const char *target = argv[argc - 2];
         const char *linkname = argv[argc - 1];
         run_ln(target, linkname, symbolic);
-    } else if (strcmp(baseName, "grep") == 0 && argc >= 4) {
+    } else if (strcmp(baseName, "grep") == 0 && argc >= 3) {
         run_grep(argv[2], argv[3]);
-    } else if (strcmp(baseName, "wc") == 0 && argc == 3) {
+    } else if (strcmp(baseName, "wc") == 0 && argc == 2) {
         run_wc(argv[2]);
     } else {
         printf("Executable name or command not recognized");
