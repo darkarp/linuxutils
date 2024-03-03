44c8c1b763a475ba834576d9f0e9944495abbb98
Mário Nascimento <mario@whitehathacking.tech>
2024-03-03 02:24:45 +0000

    fix main entries

--- src/main.c
+++ src/main.c
@@ -6,8 +6,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "../lib/headers/cat.h"
+#include "../lib/headers/fsearch.h"
+#include "../lib/headers/grep.h"
+#include "../lib/headers/ln.h"
 #include "../lib/headers/ls.h"
 #include "../lib/headers/snip.h"
+#include "../lib/headers/wc.h"
 
 int main(int argc, char *argv[]) {
     char *progName = argv[0];
@@ -32,30 +37,30 @@ int main(int argc, char *argv[]) {
             } else {
                 printf("\033[1;31mInvalid command or arguments.\033[0m\n");
             }
-        } else if (strcmp(argv[1], "fsearch") == 0 && argc == 4) {
-            run_fsearch(argv[2], argv[3]);
-        } else if (strcmp(argv[1], "cat") == 0 && argc == 3) {
-            run_cat(argv[2]);
-        } else if (strcmp(argv[1], "ln") == 0) {
-            if (argc != 5) {
-                printf("Usage: %s ln [-s] <target> <linkname>\n", argv[0]);
-                return 1;
-            }
-            int symbolic = strcmp(argv[2], "-s") == 0 ? 1 : 0;
-            const char *target = argv[argc - 2];
-            const char *linkname = argv[argc - 1];
-            run_ln(target, linkname, symbolic);
-        } else if (strcmp(argv[1], "grep") == 0 && argc >= 4) {
-            run_grep(argv[2], argv[3]);
-        } else if (strcmp(argv[1], "wc") == 0 && argc == 3) {
-            run_wc(argv[2]);
         } else {
             printf("\033[1;31mUsage:\033[0m\n  %s add <title> <content>\n  %s "
                    "list\n  %s search <keyword>\n",
                    argv[0], argv[0], argv[0]);
         }
+    } else if (strcmp(argv[1], "fsearch") == 0 && argc == 4) {
+        run_fsearch(argv[2], argv[3]);
+    } else if (strcmp(argv[1], "cat") == 0 && argc == 3) {
+        run_cat(argv[2]);
+    } else if (strcmp(argv[1], "ln") == 0) {
+        if (argc != 5) {
+            printf("Usage: %s ln [-s] <target> <linkname>\n", argv[0]);
+            return 1;
+        }
+        int symbolic = strcmp(argv[2], "-s") == 0 ? 1 : 0;
+        const char *target = argv[argc - 2];
+        const char *linkname = argv[argc - 1];
+        run_ln(target, linkname, symbolic);
+    } else if (strcmp(argv[1], "grep") == 0 && argc >= 4) {
+        run_grep(argv[2], argv[3]);
+    } else if (strcmp(argv[1], "wc") == 0 && argc == 3) {
+        run_wc(argv[2]);
     } else {
-        // TODO: others
+        printf("Executable name or command not recognized");
     }
 
     return 0;
